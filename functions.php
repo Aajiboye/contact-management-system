5931b55d6e34b959e501162652ae6dc91cf9fa92
abraham <abrahamajiboye106@gmail.com>
2020-02-20 14:51:44 +0100

    push to master

--- functions.php
+++ functions.php
@@ -1,60 +1,187 @@
 <?php
-//ALIBABA API
+//PHONEBOOK API
 //JSON_ENCODE GETS  AN ARRAY AND ENCODES IT INTO JSON FORMAT AND JSON_DECODES DOES THE  OPPOSITE
-//$phonebook = json_decode(file_get_contents("phonebook.json"));
+$phonebook = json_decode(file_get_contents("phonebook.json"));
 //file_get_contents gets values as string from file
 //json_decode converts to array
-use AlibabaCloud\Client\AlibabaCloud;
-use AlibabaCloud\Client\Exception\ClientException;
-use AlibabaCloud\Client\Exception\ServerException;
 function feedback($success = false, $message = [], $result = [])
 {
     $response = [
-        "statuscode" => $success,
-        "data" => $message,
-        "message" => $result,
+        "success" => $success,
+        "message" => $message,
+        "results" => $result,
     ];
 
     echo json_encode($response);
 }
 
-function generateUrl()
+function checkNumberExistence($phone)
 {
-    //API CALL TO ALIBABA
+    global $phonebook;
+//check by number if contact already exists
+    foreach ($phonebook as $contact) {
+        if (in_array($phone, $contact)) {
+            $status = true;
+            break;
+        } else {
+            $status = false;
+        }
 
-// Download：https://github.com/aliyun/openapi-sdk-php
-// Usage：https://github.com/aliyun/openapi-sdk-php/blob/master/README.md
-
-AlibabaCloud::accessKeyClient('LTAI4FthQdW99VGCefrVdHgW', '6dp7tHtTxWDQAi6N3nY8NBbexiyqpC')
-                        ->regionId('cn-hangzhou')
-                        ->asDefaultClient();
-
-try {
-    $result = AlibabaCloud::rpc()
-                          ->product('live')
-                          // ->scheme('https') // https | http
-                          ->version('2016-11-01')
-                          ->action('ResumeLiveStream')
-                          ->method('POST')
-                          ->host('live.aliyuncs.com')
-                          ->options([
-                                        'query' => [
-                                          'RegionId' => "cn-hangzhou",
-                                          'LiveStreamType' => "publisher",
-                                          'AppName' => "kols",
-                                          'StreamName' => "kolid111",
-                                          'DomainName' => "ingest.producekolinjeststream.com",
-                                        ],
-                                    ])
-                          ->request();
-    print_r($result->toArray());
-} catch (ClientException $e) {
-    echo $e->getErrorMessage() . PHP_EOL;
-} catch (ServerException $e) {
-    echo $e->getErrorMessage() . PHP_EOL;
+    }
+    return $status;
 }
-    //Save response
+function addContact($name='', $phone='', $email='', $gender='', $bio='')
+{
+    //GLOBAL KEYWORD MAKES PHONEBOOK VISIBLE INSIDE THE FUNCTION
+    global $phonebook;
+    $name = ucfirst($name);
+    $success = false;
+    if ($name == "") {
+        $message[] = 'Name field is required!';
+    }
+    if ($phone == "") {
+        $message[] = 'Number field cannot be empty';
+    }
+    if (!(is_numeric($phone))) {
+        $message[] = 'Number field cannot contain letters';
+    }
+    if (!preg_match("/^[a-zA-Z0-9 ]*$/", $name)) {
+        $message[] = "Only letters and white space allowed";
+    }
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $message[] = "Invalid email format";
+    }
+    if ($gender == "") {
+        $message[] = 'Kindly select a gender';
+    }
 
-    //API CALL TO kols node instance
-    echo feedback(true,[],$result);
+    if ($phone != "" && $gender != "" && (is_numeric($phone)) && preg_match("/^[a-zA-Z0-9 ]*$/", $name) && filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if (checkNumberExistence($phone)) {
+            $success = false;
+            $message[] = 'Contact already exists with ' . $phone;
+        } else {
+            $contact = array($name, $phone, $email, $gender, $bio);
+            $phonebook[] = $contact;
+            savePhonebook();
+            $success = true;
+            $message[] = $name . ' has been successfully  added to your contacts';
+        }
+
+    }
+    feedback($success, $message);
+}
+
+function viewPhonebook()
+{
+    global $phonebook;
+    $success = true;
+    $result = $phonebook;
+    $message = [];
+    feedback($success, $message, $result);
+}
+
+function deleteContact($index = '')
+{
+    global $phonebook;
+    $result = [];
+    if ($index == "") {
+        $success = false;
+        $message[] = 'ID cannot be empty!';
+    } elseif (!(isset($phonebook[$index]))) {
+        $message[] = 'Contact does not exist!';
+        $success = false;
+    } else {
+        $deleted = array_splice($phonebook, $index, 1);
+        savePhonebook();
+        $success = true;
+        $message[] = $deleted[0][0] . ' has been deleted from your phonebook!';
+    }
+    feedback($success, $message, $result);
+
+}
+
+function searchFields($contact, $searchKey)
+{
+    foreach ($contact as $field) {
+        if (strpos(strtolower($field), strtolower($searchKey)) !== false) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function search($searchKey)
+{
+    global $phonebook;
+    $searchResult = [];
+    $i = 0;
+    foreach ($phonebook as $contact) {
+        $Temp = [];
+
+        if (searchFields($contact, $searchKey)) {
+            $Temp[] = $i;
+
+            foreach ($contact as $key) {
+                $Temp[] = $key;
+            }
+            $searchResult[] = $Temp;
+        }
+        $i++;
+    }
+    if (count($searchResult) == 0) {
+        $success = false;
+        $message[] = 'No contact Found';
+    } else {
+        $success = true;
+        if (count($searchResult) > 1) {
+            $message[] = 'found ' . count($searchResult) . ' results';
+        } else {
+            $message[] = 'found ' . count($searchResult) . ' result';
+        }
+
+        $result = $searchResult;
+        feedback($success, $message, $result);
+    }
+}
+function editContact($id = '', $name = '', $phone = '', $email = '', $gender = '', $bio = '')
+{
+    global $phonebook;
+    $name = ucfirst($name);
+    $result = [];
+    if (!(is_numeric($phone))) {
+        $message[] = 'Number field cannot contain letters';
+        $success = false;
+    } elseif (!(isset($phonebook[$id]))) {
+        $message[] = 'Contact does not exist';
+        $success = false;
+    } else {
+
+        if ($name == "") {
+            $name = $phonebook[$id][0];
+        }
+        if ($phone == "") {
+            $phone = $phonebook[$id][1];
+        }
+        if ($email == "") {
+            $email = $phonebook[$id][2];
+        }
+        if ($gender == "") {
+            $gender = $phonebook[$id][3];
+        }
+        if ($bio == "") {
+            $bio = $phonebook[$id][4];
+        }
+        $newContact = [$name, $phone, $email, $gender, $bio];
+        $phonebook[$id] = $newContact;
+        savePhonebook();
+        $result = $newContact;
+        $message[] = 'Contact Updated';
+        $success = true;
+    }
+    feedback($success, $message, $result);
+}
+function savePhonebook()
+{
+    global $phonebook;
+    file_put_contents('phonebook.json', json_encode($phonebook));
 }
--- index.php
+++ index.php
@@ -1,25 +1,37 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
 require 'functions.php';
 $action = !empty($_GET['action']) ? $_GET['action'] : '';
 $response = [];
 
 //switch statement based on selections
 switch ($action) {
-    case 'generateurl':
-        
-        generateUrl();
+    case 'add':
+        $name = !empty($_POST['name']) ? $_POST['name'] : ''; // Read about ternary operators
+        $phone = !empty($_POST['phone']) ? $_POST['phone'] : '';
+        $email = !empty($_POST['email']) ? $_POST['email'] : '';
+        $gender = !empty($_POST['gender']) ? $_POST['gender'] : '';
+        $bio = !empty($_POST['bio']) ? $_POST['bio'] : '';
+        addContact($name, $phone, $email, $gender, $bio);
         break;
     case 'delete':
-        
+        $id = isset($_POST['id']) ? $_POST['id'] : '';
+        deleteContact($id);
         break;
     case 'update':
-        
+        $name = !empty($_POST['name']) ? $_POST['name'] : ''; // Read about ternary operators
+        $phone = !empty($_POST['phone']) ? $_POST['phone'] : '';
+        $email = !empty($_POST['email']) ? $_POST['email'] : '';
+        $gender = !empty($_POST['gender']) ? $_POST['gender'] : '';
+        $bio = !empty($_POST['bio']) ? $_POST['bio'] : '';
+        $id = isset($_POST['id']) ? $_POST['id'] : '';
+
+        editContact($id, $name, $phone, $email, $gender, $bio);
         break;
     case 'search':
-        
+        $searchKey = $_POST['keyword'];
+        search($searchKey);
         break;
     case 'view':
-        
+        viewPhonebook();
         break;
 };
